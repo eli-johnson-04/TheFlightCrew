7af65d4d007229fd9f89104e95cd98527136fa9c
eli-johnson-04 <elijah.johnson@ufl.edu>
2023-12-02 16:03:55 -0500

    attempt at getting rid of layovers
    most of the layover removal stuff works, but there are extraneous problems with typos

--- data_processing/data_processing_script.py
+++ data_processing/data_processing_script.py
@@ -8,8 +8,9 @@ def fixCSV(airlineReviews, newfile):
     "moldovia", "aviation", "tel avia", "latvia", "monrovia", "avianca", "smartavia", "transavia", "peruvian",
     "viaair", "boliviana", "belavia"]
 
+
     # This pattern searches for "via" and any word following. We do not care about layovers.
-    layoverPattern = re.compile(r'\bvia\b\s+(?!' + '|'.join(map(re.escape, exclude_list)) + r')\w+', flags=re.IGNORECASE)
+    layoverPattern = re.compile(r'(((.*)(\s+to\s+)(.*)|[\w]+-[A-Z]+))\s+via\s+(\b\w+\b)\s*', flags = re.IGNORECASE)
 
     # Open the output file with the specifed name.
     with open(newfile, mode = 'w', encoding = 'utf-8', newline = '') as newFile:
@@ -30,15 +31,22 @@ def fixCSV(airlineReviews, newfile):
 
                 # Otherwise, check for layovers in the "Route" column (10).
                 else:
-                    if layoverPattern.search(review[10]):
-                        # Get rid of the layover information.
-                        review[10] = layoverPattern.sub('', review[10]).strip()
+                    # Get the match.
+                    match = re.search(layoverPattern, review[10])
+                    if match:
+                        # The city we need to check is the one before "via" and it exists in group 5
+                        via_city = match.group(5)
+
+                        # Check if the city contains "via" when it is not supposed to. This is basically a typo check.
+                        if via_city and ("via" in via_city) and (via_city not in exclude_list):
+                            review[10] = match.group(3) + match.group(4) + match.group(5)[0 : match.group(5).find('via')]
+                        else:
+                            review[10] = match.group(1)
                     writer.writerow(review)
                 row += 1
-                #TODO: fix shitty data entry ????
 
 def main():
-    fixCSV('AirlineReviews.csv', 'test.csv')
+    fixCSV('AirlineReviews.csv', 'test_12-2-23.csv')
     exit()
 
 if __name__ == '__main__':
