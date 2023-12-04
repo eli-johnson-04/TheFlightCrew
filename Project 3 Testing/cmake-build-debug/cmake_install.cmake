67bc461a28477bf941596ee324a8631c48a56365
Jonathan X <115114569+jxavioer@users.noreply.github.com>
2023-12-04 18:22:07 -0500

    Finalized Submission

--- Project 3 Testing/cmake-build-debug/cmake_install.cmake
+++ Project 3 Testing/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/jbeni/OneDrive/Documents/GitHub/TheFlightCrew/Project 3 Testing
+# Install script for directory: C:/Users/jxavi/OneDrive/Documents/GitHub/TheFlightCrew/Project 3 Testing
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Airline_Processing")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/NoRoomForAiror")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files (x86)/JetBrains/CLion 2022.2.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.1/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/jbeni/OneDrive/Documents/GitHub/TheFlightCrew/Project 3 Testing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jxavi/OneDrive/Documents/GitHub/TheFlightCrew/Project 3 Testing/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
